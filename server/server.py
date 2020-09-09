9e622f1e438f1ddc0f23568cbe4388f4b3e00548
Arnab Sagar <arnab.sagar47@gmail.com>
2020-09-09 16:26:22 -0400

    linked the new files

--- server/server.py
+++ server/server.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 print(__name__)
 
 
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
+
 @app.route('/<username>/<int:post_id>')  #decorator
 def hello_world(username=None, post_id = None):
     return render_template('index.html', name=username, post_id=post_id)
