c8616bff2bc59c7e9333571428d891ed7e2e207f
arnabsagar <arnab.sagar47@gmail.com>
2020-09-25 10:43:20 -0400

    added code

--- server/server.py
+++ server/server.py
@@ -3,21 +3,18 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 print(__name__)
 
-
 @app.route('/')
 def landing_page():
     return render_template('index.html')
 
-@app.route('/<username>/<int:post_id>')  #decorator
-def hello_world(username=None, post_id = None):
-    return render_template('index.html', name=username, post_id=post_id)
-
 @app.route('/resume')
+def resume():
+    return render_template('resume.pdf')
 
-# @app.route('/favicon.ico')
+# @app.route('/favsicon.ico')
 # def blog():
 #     return 'what@ss'
 
-@app.route('/blog/of/my/dog')
+@app.route('/blog')
 def dogs():
-    return "my doggo!"
+    return "blog page"
