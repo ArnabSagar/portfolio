40a170e7956f013ff308bd394931ce0c7dfdef96
arnabsagar <arnab.sagar47@gmail.com>
2020-09-25 11:05:34 -0400

    added pdf code, still doesnt work

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, send_file
 
 app = Flask(__name__)
 print(__name__)
@@ -8,9 +8,9 @@ def landing_page():
     return render_template('index.html')
 
 @app.route('/resume')
-def resume():
-    return render_template('resume.pdf')
-
+def resume(filename = None):
+    with open('./static/Sagar_Arnab_F20.pdf', 'rb') as static_file:
+        return send_file(static_file, attachment_filename="Sagar_Arnab_F20.pdf")
 # @app.route('/favsicon.ico')
 # def blog():
 #     return 'what@ss'
